d3e83a9848fda17d997daabc24dd841dfd116503
Azmal Karim <azmalsirat@gmail.com>
2025-07-03 12:09:23 +0600

    connected at local successfully

--- frontend/wealthe-frontend/src/utils/api.js
+++ frontend/wealthe-frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 
 //import { data } from "react-router";
-// const API_BASE_URL = 'http://localhost:8081';
+const API_BASE_URL = 'http://localhost:8081';
 //const API_BASE_URL = 'http://172.174.246.178:8081'; // Adjust this to your backend URL
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://172.174.246.178:8081'; 
+// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://172.174.246.178:8081'; 
 
 
 export const apiCall = async (endpoint, options = {}) => {
