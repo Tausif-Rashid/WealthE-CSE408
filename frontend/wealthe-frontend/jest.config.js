a436b9c4e7273210b4392c6a54786c73c2fae873
Tausif Rashid <91827238+Tausif-Rashid@users.noreply.github.com>
2025-07-09 10:35:32 +0600

    try2

--- frontend/wealthe-frontend/jest.config.js
+++ frontend/wealthe-frontend/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   testPathIgnorePatterns: [
     "/node_modules/",
-    "src/__tests__/utils/",     // adjust this to your path
+    "/__tests__/utils/",     // adjust this to your path
     "src/__tests__/mocks/",     // another common case
-    "src/__tests__/setup/",
+    "src/__tests__/setup/testUtils.js", // adjust this to your path
   ],
 };
