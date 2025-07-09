098a10fa717671832f3840551f0f85e200920f58
Tausif Rashid <91827238+Tausif-Rashid@users.noreply.github.com>
2025-07-09 10:24:00 +0600

    try jest config

---
+++ frontend/wealthe-frontend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "src/__tests__/utils/",     // adjust this to your path
+    "src/__tests__/mocks/",     // another common case
+    "src/__tests__/setup/",
+  ],
+};
